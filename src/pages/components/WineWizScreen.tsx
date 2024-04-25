2caa337197e53336d5b14a1c47a0493520eb2d20
LeoXUWEI <xuwei_leo@163.com>
2024-04-24 17:37:41 -0700

    Button Font Fixed

--- src/pages/components/WineWizScreen.tsx
+++ src/pages/components/WineWizScreen.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { useState } from 'react'
-import {ScreenProps} from "@/types/Screen.props";
+import { ScreenProps } from "@/types/Screen.props";
 import SwitchButton from '@/components/switchButton';
 import useDisplayWord from '@/hooks/useDisplayWord'
 import { setTimeout } from "timers/promises";
 
-const WineWizScreen: React.FC<ScreenProps> = ({toNextScreen}) => {
-    
+const WineWizScreen: React.FC<ScreenProps> = ({ toNextScreen }) => {
+
     let origintext = [
-        'Hi, l am wiz, your personal wine master.',
-        'i can pick the perfect gift wine for you with just a few questions answered.Afterwards,i will help you budget wines create personalized gift cards to make your gift special!'
+        'Hi, I am Wiz, your personal wine master. ',
+        'I can pick the perfect gift wine for you  with just a few questions answered. Afterwards, I will help you create personalized gift cards to make your gift special!'
     ]
-    const {displayTexts} = useDisplayWord(origintext)
+    const { displayTexts } = useDisplayWord(origintext)
     const [showAll, setShowAll] = useState(false)
-    const [customObjContent, setCustomObjContent] = useState<{className: string, text: string, onClick: Function}[]>([{
+    const [customObjContent, setCustomObjContent] = useState<{ className: string, text: string, onClick: Function }[]>([{
         className: 'pinot',
         text: 'Continue',
         onClick: handleContinue
     }])
-    function handleContinue(){
+    function handleContinue() {
         setShowAll(true)
         window.setTimeout(() => {
             toNextScreen()
@@ -27,9 +27,9 @@ const WineWizScreen: React.FC<ScreenProps> = ({toNextScreen}) => {
     return (
         <>
             <div>
-                <h1 className={'text-[#6B003A] text-3xl text-center font-bold pt-10'}>WineWiz</h1>
-                { (showAll ? origintext : displayTexts).map((item: string, index: number) => (
-                    <div className={'text-left mt-3 text-[#6B003A] font-bold pl-5 pr-5 w-screen font-1'} key={index}>{item}</div>
+                <h1 className={'text-[#6B003A] text-[24px] font_page_title text-center pt-10'}>WineWiz</h1>
+                {(showAll ? origintext : displayTexts).map((item: string, index: number) => (
+                    <div className={'text-[#6B003A] text-[14px] font_paragraph text-left mt-3 pl-5 pr-5 w-screen'} key={index}>{item}</div>
                 ))}
             </div>
             <div className={'w-80 mx-auto'}>
--- src/pages/components/WineWizeIndex.tsx
+++ src/pages/components/WineWizeIndex.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import {ScreenProps} from "@/types/Screen.props";
+import { ScreenProps } from "@/types/Screen.props";
 
 
-const WineWizeIndex: React.FC<ScreenProps> = ({toNextScreen}) => {
+const WineWizeIndex: React.FC<ScreenProps> = ({ toNextScreen }) => {
     return (
         <div className={"content flex-row"}>
             <div className={"enterShow flex flex-col justify-center pt-36"}>
-                <img
+                {/* <img
                     className={"image_logo pb-12"}
                     src={'/WineWiz.png'}
                     alt="WineWiz"
-                />
+                /> */}
+                <p
+                    className={'text-center pb-12 text-[#6B003A] text-[64px] font_product_title'}>WineWiz
+                </p>
+
                 <img
                     className={"image_logo m-auto w-40"}
                     src={'/WineWizICon.png'}
@@ -19,7 +23,8 @@ const WineWizeIndex: React.FC<ScreenProps> = ({toNextScreen}) => {
             </div>
             <p
                 onClick={toNextScreen}
-                className={'text-center mt-10 text-[#6B003A] font-bold font-2'}>Tap to conitinue</p>
+                className={'text-center mt-10 text-[#6B003A] text-[18px] font_button_text'}>tap to continue
+            </p>
         </div>
     )
 }
