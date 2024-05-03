ebbd150516213be0f620d21aa23c1fd062803694
LeoXUWEI <xuwei_leo@163.com>
2024-05-03 00:47:12 -0700

    Update WizThinkingScreen.tsx

--- src/pages/components/WizThinkingScreen.tsx
+++ src/pages/components/WizThinkingScreen.tsx
@@ -28,7 +28,7 @@ const WizThinkingScreen: React.FC<ScreenProps> = ({ toNextScreen }) => {
 
     return (
         <>
-            <div className={'text-[#6B003A] text-[24px] font_normal_bold text-center pt-80'}>{displayTexts[0]}</div>
+            <div className={'text-[#fff] text-[24px] font_normal_bold text-center pt-80'}>{displayTexts[0]}</div>
         </>
     )
 }
