4c073786d567736523571f483e798956bc5389e0
LeoXUWEI <xuwei_leo@163.com>
2024-05-05 04:49:19 -0700

    图片尺寸修改

--- utils/openai.js
+++ utils/openai.js
@@ -119,7 +119,7 @@ export async function createImage(prompt) {
     model: "dall-e-3",
     prompt: prompt,
     n: 1,
-    size: "1024x1024",
+    size: "1200x900",
   });
 
   return response.data[0].url;
