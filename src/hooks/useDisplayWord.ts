cb5a67e6e1959a38cac0faca87fdc01ea103129c
LeoXUWEI <xuwei_leo@163.com>
2024-05-05 01:03:14 -0700

    按钮不点击不进入下一页问题

--- src/hooks/useDisplayWord.ts
+++ src/hooks/useDisplayWord.ts
@@ -30,7 +30,7 @@ const useDisplayWord = (texts: Array<string>) => {
           setCharIndex(0);
         }
       }
-    }, 10); 
+    }, 20); 
 
     return () => clearTimeout(timeout);
   }, [texts, textIndex, charIndex]); 
