b2b3584d2b1b1d337d3cb31c740f6c2b6141ec98
LeoXUWEI <xuwei_leo@163.com>
2024-05-05 01:06:35 -0700

    字幕速度

--- src/hooks/useDisplayWord.ts
+++ src/hooks/useDisplayWord.ts
@@ -30,7 +30,7 @@ const useDisplayWord = (texts: Array<string>) => {
           setCharIndex(0);
         }
       }
-    }, 20); 
+    }, 60); 
 
     return () => clearTimeout(timeout);
   }, [texts, textIndex, charIndex]); 
