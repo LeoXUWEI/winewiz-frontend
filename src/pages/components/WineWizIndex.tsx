f8b58571bf856421b04ecc36e83e180179d71f03
LeoXUWEI <xuwei_leo@163.com>
2024-05-06 05:19:10 -0700

    首页logo更新

--- src/pages/components/WineWizIndex.tsx
+++ src/pages/components/WineWizIndex.tsx
@@ -6,19 +6,17 @@ const WineWizIndex: React.FC<ScreenProps> = ({ toNextScreen, handleWaveHeight })
     return (
         <div onClick={toNextScreen} className={"content flex-row w-screen h-screen"}>
             <div className={"enterShow flex flex-col justify-center pt-36"}>
-                {/* <img
-                    className={"image_logo pb-12"}
+
+                <img
+                    className={"image_logo m-auto mb-8 w-60"}
                     src={'/WineWiz.png'}
-                    alt="WineWiz"
-                /> */}
-                <p
-                    className={'text-center pb-12 text-[#6B003A] text-[64px] font_extra_bold'}>WineWiz
-                </p>
+                    alt="WineWizICon"
+                />
 
                 <img
                     className={"image_logo m-auto w-40"}
                     src={'/WineWizICon.png'}
-                    alt="WineWiz"
+                    alt="WineWizICon"
                 />
             </div>
             <p
