3bf62736e9620e5e28a6ec5feb55bab7fa0874b0
LeoXUWEI <xuwei_leo@163.com>
2024-05-06 09:44:49 -0700

    Update makeGiftCard.tsx

--- src/pages/makeGiftCard.tsx
+++ src/pages/makeGiftCard.tsx
@@ -92,17 +92,17 @@ export default function MakeGiftCard({ toNextScreen }: { toNextScreen: any }) {
     if (captureElement) {
       const canvas = await html2canvas(captureElement)
       if (canvas) {
-// // imgUrl 是图片的 base64格式 代码 png 格式
-const imgUrl = canvas.toDataURL('image/png');
-// //下载图片的功能。
-downloadIamge(imgUrl, "plantCardImg.png")
+        // // imgUrl 是图片的 base64格式 代码 png 格式
+        const imgUrl = canvas.toDataURL('image/png');
+        // //下载图片的功能。
+        downloadIamge(imgUrl, "plantCardImg.png")
       }
-        
-      
+
+
     }
   };
 
-  function downloadIamge(imgsrc:string, name:string) {  //下载图片地址和图片名
+  function downloadIamge(imgsrc: string, name: string) {  //下载图片地址和图片名
     const image = new Image();
     image.setAttribute("crossOrigin", "anonymous");
     image.onload = function () {
@@ -112,12 +112,12 @@ downloadIamge(imgUrl, "plantCardImg.png")
       const context = canvas.getContext("2d");
       if (context) {
         context.drawImage(image, 0, 0, image.width, image.height);
-      const url = canvas.toDataURL("image/png"); //得到图片的base64编码数据
-      const a = document.createElement("a"); // 生成一个a元素
-      const event = new MouseEvent("click"); // 创建一个单击事件
-      a.download = name || "photo"; // 设置图片名称
-      a.href = url; // 将生成的URL设置为a.href属性
-      a.dispatchEvent(event); // 触发a的单击事件
+        const url = canvas.toDataURL("image/png"); //得到图片的base64编码数据
+        const a = document.createElement("a"); // 生成一个a元素
+        const event = new MouseEvent("click"); // 创建一个单击事件
+        a.download = name || "photo"; // 设置图片名称
+        a.href = url; // 将生成的URL设置为a.href属性
+        a.dispatchEvent(event); // 触发a的单击事件
       }
     };
     image.src = imgsrc;
