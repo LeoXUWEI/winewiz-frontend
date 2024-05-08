1983f4ad5a1d64d8b4b91b4141d53585ef7eaddf
yang <975965279@qq.com>
2024-05-09 00:23:19 +0800

    Delete demo-api.ts

--- src/pages/api/demo-api.ts
+++
@@ -1,9 +0,0 @@
-
-
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    // 返回一个简单的 JSON 响应
-    res.status(200).json({ message: 'This is a demo API response!' });
-  };
